d859af9f5d9130daeeec274f293592f05b95b2e5
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-07-20 11:33:20 +0000

    underconstruction

--- src/app.py
+++ src/app.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+
+# Create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
@@ -20,25 +21,24 @@ jackson_family = FamilyStructure("Jackson")
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
+# Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
 
-    # this is how you can use the Family datastructure by calling its methods
+@app.route('/members', methods=['GET', 'POST'])
+def handle_family_members():
+    # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = { "hello": "world",
+                      "family": members}
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
+# This only runs if `$ python src/app.py` is executed
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
